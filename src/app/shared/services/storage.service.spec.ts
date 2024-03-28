50015d60f2a83c1750968bc0efdf432661f62722
Kristofer Kimotho <kristofer.kimotho@equitybank.co.ke>
2024-03-28 12:49:55 +0300

    Wrote methods for RxDB in storage service - Declared relevant interfaces and types to aid with intellisense - PENDING: Fille attachments to documents

--- src/app/shared/services/storage.service.spec.ts
+++ src/app/shared/services/storage.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
+import { isRxDatabase } from 'rxdb';
 
 describe('StorageService', () => {
   let service: StorageService;
@@ -8,53 +9,65 @@ describe('StorageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StorageService);
-    localStorage.clear();
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('Local Storage', () => {
+    beforeEach(() => {
+      localStorage.clear();
+    });
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    it('should write to storage', () => {
+      const testObject = { key: 1 };
+      const testKey = 'key';
+
+      service.writeToStorage(testKey, testObject);
+
+      let storedItem = localStorage.getItem(testKey);
+      let parsedItem: any = undefined;
+      if (storedItem) parsedItem = JSON.parse(storedItem);
+
+      expect(parsedItem).toBeDefined();
+      expect(parsedItem['key']).toEqual(testObject['key']);
+    });
+
+    it('should read from storage', () => {
+      const testObject = { key: 1 };
+      const testKey = 'key';
+
+      localStorage.setItem(testKey, JSON.stringify(testObject));
+
+      let readItem = service.readFromStorage(testKey);
+
+      expect(readItem).not.toBeNull();
+      expect(readItem).not.toBeUndefined();
+      expect(readItem['key']).toEqual(testObject['key']);
+    });
+
+    it('should delete an item', () => {
+      const testObject = { key: 1 };
+      const testKey = 'key';
+
+      localStorage.setItem(testKey, JSON.stringify(testObject));
+
+      service.delete(testKey);
+
+      let storedItem = localStorage.getItem(testKey);
+      let parsedItem: any = undefined;
+      if (storedItem) parsedItem = JSON.parse(storedItem);
+
+      expect(storedItem).toBeNull();
+      expect(parsedItem).toBeUndefined();
+    });
   });
 
-  it('should write to storage', () => {
-    const testObject = { key: 1 };
-    const testKey = 'key';
-
-    service.writeToStorage(testKey, testObject);
-
-    let storedItem = localStorage.getItem(testKey);
-    let parsedItem: any = undefined;
-    if (storedItem) parsedItem = JSON.parse(storedItem);
-
-    expect(parsedItem).toBeDefined();
-    expect(parsedItem['key']).toEqual(testObject['key']);
-  });
-
-  it('should read from storage', () => {
-    const testObject = { key: 1 };
-    const testKey = 'key';
-
-    localStorage.setItem(testKey, JSON.stringify(testObject));
-
-    let readItem = service.readFromStorage(testKey);
-
-    expect(readItem).not.toBeNull();
-    expect(readItem).not.toBeUndefined();
-    expect(readItem['key']).toEqual(testObject['key']);
-  });
-
-  it('should delete an item', () => {
-    const testObject = { key: 1 };
-    const testKey = 'key';
-
-    localStorage.setItem(testKey, JSON.stringify(testObject));
-
-    service.delete(testKey);
-
-    let storedItem = localStorage.getItem(testKey);
-    let parsedItem: any = undefined;
-    if (storedItem) parsedItem = JSON.parse(storedItem);
-
-    expect(storedItem).toBeNull();
-    expect(parsedItem).toBeUndefined();
+  describe('RXDB', () => {
+    describe('createRxDatabase()', () => {
+      it('should initialize on service initialization', () => {
+        expect(isRxDatabase(service._database)).toBeDefined;
+      });
+    });
   });
 });
--- src/app/shared/services/storage.service.ts
+++ src/app/shared/services/storage.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
+
 import { RxDatabase, addRxPlugin } from 'rxdb';
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
-addRxPlugin(RxDBDevModePlugin);
+import { RxDBAttachmentsPlugin } from 'rxdb/plugins/attachments';
 
 import { createRxDatabase } from 'rxdb';
 import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
@@ -11,19 +12,6 @@ import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
 })
 export class StorageService implements OnInit {
   constructor() {}
-
-  private database?: RxDatabase;
-
-  async ngOnInit() {
-    this.database = await this.createRxDatabase();
-  }
-
-  writeToStorage(key: string, item: any) {
-    // Don't JSON stringigy data that is already a string
-    if (typeof item == 'string') return localStorage.setItem(key, item);
-
-    return localStorage.setItem(key, JSON.stringify(item));
-  }
 
   readFromStorage(key: string) {
     let item = localStorage.getItem(key);
@@ -57,6 +45,24 @@ export class StorageService implements OnInit {
   RXDB CODE
   */
 
+  _database?: RxDatabase;
+
+  async ngOnInit() {
+    addRxPlugin(RxDBAttachmentsPlugin);
+    addRxPlugin(RxDBDevModePlugin);
+    this._database = await this.createRxDatabase();
+  }
+
+  writeToStorage(key: string, item: any) {
+    // Don't JSON stringigy data that is already a string
+    if (typeof item == 'string') return localStorage.setItem(key, item);
+
+    return localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  private readonly DOCUMENT_NOT_FOUND = 'Document not found';
+  private readonly COLLECTION_NOT_FOUND = 'Collection not found';
+
   private createRxDatabase(name: string = 'recipe-reverie') {
     return createRxDatabase({
       name: name,
@@ -66,37 +72,82 @@ export class StorageService implements OnInit {
 
   // Collection CRUD
 
-  createRxCollection(collectionName: string, collectionSchema: RxDBSchema) {
-    return this.database?.addCollections({
+  createRxCollection(
+    collectionName: string,
+    collectionSchema: RxDBSchema | RxDBWithAttachmentsSchema
+  ) {
+    return this._database?.addCollections({
       [collectionName]: {
         schema: collectionSchema,
       },
     });
   }
 
-  getRxCollection(name: string) {
-    return this.database?.[name];
+  private _getRxCollection(name: string) {
+    return this._database?.[name];
+  }
+
+  getCollection(name: string) {
+    return this._getRxCollection(name);
+  }
+  getCollection$(name: string) {
+    return this.getCollection(name)?.$;
+  }
+
+  deleteCollection(name: string) {
+    return this._getRxCollection(name)?.remove();
   }
 
   // Document CRUD
-  async createDocument(collectionName: string, document: any) {
-    return this.database?.[collectionName].insert(document);
+  createDocument(collectionName: string, document: any) {
+    return this._getRxCollection(collectionName)?.insert(document);
   }
 
-  async createManyDocuments(collectionName: string, documents: any[]) {
-    return this.database?.[collectionName].bulkInsert(documents);
+  createManyDocuments(collectionName: string, documents: any[]) {
+    return this._getRxCollection(collectionName)?.bulkInsert(documents);
   }
 
-  readDocument(collectionName: string, primaryKeyValue: string) {
-    const collection = this.getRxCollection(collectionName);
-    if (!collection) throw new Error('Collection does not exist');
-    return collection.findByIds([primaryKeyValue]);
+  getDocument(collectionName: string, primaryKeyValue: DocumentIndexType) {
+    const collection = this._getRxCollection(collectionName);
+    if (!collection) throw new Error(this.COLLECTION_NOT_FOUND);
+
+    if (typeof primaryKeyValue == 'string') {
+      return collection.findOne(primaryKeyValue);
+      // return collection.findByIds([primaryKeyValue]);
+    } else {
+      const id = collection.schema.getPrimaryOfDocumentData(primaryKeyValue);
+      return collection.findOne(id);
+    }
+  }
+
+  getDocument$(collectionName: string, primaryKeyValue: string) {
+    return this.getDocument(collectionName, primaryKeyValue).$;
+  }
+
+  updateDocument(
+    collectionName: string,
+    primaryKeyValue: string,
+    updates: SchemaPrimaryKey
+  ) {
+    const doc = this.getDocument(collectionName, primaryKeyValue);
+    if (!doc || !doc.exists) throw new Error(this.DOCUMENT_NOT_FOUND);
+    return doc.update(updates);
+  }
+
+  deleteManyDocuments(collectionName: string, keys: string[]) {
+    return this._getRxCollection(collectionName)?.bulkRemove(keys);
+  }
+
+  deleteDocument(collectionName: string, primaryKeyValue: string) {
+    return this.getDocument(collectionName, primaryKeyValue).remove();
   }
 }
 
+// Interfaces and types for RxDB
+
 export interface RxDBSchema {
   version: number;
-  primaryKey: string;
+  primaryKey: SchemaPrimaryKey;
   type: string;
   properties: {
     [keys: string]: {
@@ -105,4 +156,52 @@ export interface RxDBSchema {
     };
   };
   required: string[];
+}
+export interface RxDBWithAttachmentsSchema extends RxDBSchema {
+  attachments: {
+    encrypted: boolean;
+  };
+}
+
+type SchemaPrimaryKey =
+  | string
+  | { key: string; fields: string[]; separator: string };
+
+type DocumentIndexType = string | { [keys: string]: any };
+
+export interface RxDBDocUpdateOperators {
+  // Fields operators
+  $currentDate?: {
+    [keys: string]: boolean | { $type: 'timestamp' } | { $type: 'date' };
+  };
+  $inc?: {
+    [keys: string]: number;
+  };
+  $min?: {
+    [keys: string]: any;
+  };
+  $max?: {
+    [keys: string]: any;
+  };
+  $mul?: {
+    [keys: string]: number;
+  };
+  $rename?: {
+    [keys: string]: string;
+  };
+  $set?: {
+    [keys: string]: any;
+  };
+  $setOnInsert?: {
+    [keys: string]: any;
+  };
+  $unset?: {
+    [keys: string]: any;
+  };
+}
+
+export interface documentAttachment {
+  id: string;
+  data: string | Blob;
+  type: string;
 }
